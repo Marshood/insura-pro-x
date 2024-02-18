27243880709ca17277fb894d3257ef9a0fef8d5d
Marshood Ayoub <marshood.ayoub@ridewithvia.com>
2024-02-18 23:51:53 +0200

    fix useTranslations

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,17 +1,11 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import Link from "next/link";
-  
-const inter = Inter({ subsets: ["latin"] });
 
 export default function error() {
-   return (
+  return (
     <div className="flex justify-center flex-col">
       <h1>Not Found</h1>
       <p>Could not find requested resource</p>
       <Link href="/">Return Home</Link>
-     </div>
+    </div>
   );
-}
- 
 }
