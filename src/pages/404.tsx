05c010618abce88fc242df491dc01a3a13d66214
Marshood Ayoub <marshood.ayoub@ridewithvia.com>
2024-02-18 23:49:00 +0200

    fix  React Hook

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,26 +1,17 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Link from "next/link";
-import { GetStaticPropsContext } from "next/types";
-import { useTranslations } from "next-intl";
-
+  
 const inter = Inter({ subsets: ["latin"] });
 
 export default function error() {
-  const t = useTranslations();
-  return (
+   return (
     <div className="flex justify-center flex-col">
       <h1>Not Found</h1>
       <p>Could not find requested resource</p>
       <Link href="/">Return Home</Link>
-      <p>{t("workCompleteFrom")}</p>
-    </div>
+     </div>
   );
 }
-export async function getStaticProps({ locale }: GetStaticPropsContext) {
-  return {
-    props: {
-      messages: (await import(`../../locales/${locale}.json`)).default,
-    },
-  };
+ 
 }
